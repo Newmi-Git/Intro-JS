da3c773369bc2ea6ef58fd5008422c5c3152dfc9
Newmi-Git <yaghyaabdul1006@gmail.com>
2026-05-26 14:27:22 +0200

    added button message

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 const button = document.querySelector("button");
+const heading = document.querySelector("h1");
+const paragraph = document.querySelector("p")
+
+button.addEventListener("click", function() {
+    const name = prompt("What is your name?");
+    const mood = prompt("Whats Todays Mood?");
+
+    heading.textContent = `Hello, ${name}!`;
+    paragraph.textContent = `Todays Mood is: ${mood}`;
+        
+    
+    
+    console.log("The button has been clicked");
+    button.textContent = "Clicked!";
+    
+    console.log("Both Mood and Name was Succesfully Entered!")
+    
+})
+
